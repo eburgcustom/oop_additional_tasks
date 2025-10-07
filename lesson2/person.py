fec77152a29f2ad654f10e7b4b501fd9ac54d08b
Ildar <gif8686@gmail.com>
2025-10-07 12:47:23 +0500

    person.py ready

--- lesson2/person.py
+++ lesson2/person.py
@@ -8,10 +8,31 @@
 - is_adult(cls, age): статический метод, принимающий возраст человека и возвращающий True,
 если он старше 18 лет, и False в противном случае
 """
-
+from datetime import date
 
 class Person:
-    pass
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+
+    def display(self):
+        print(f'{self.name} is {self.age} years old')
+
+
+    @classmethod
+    def from_birth_year(cls, name, birth_year):
+        current_year = date.today().year
+        age = current_year - birth_year
+        return cls(name, age)
+
+
+    @staticmethod
+    def is_adult(age):
+        if age >= 18:
+            return True
+        else:
+            return False
 
 
 # код для проверки 
