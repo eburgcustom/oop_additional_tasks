df47f06ca82907258e709331943c76c96238e5f1
Ildar <gif8686@gmail.com>
2025-10-13 19:53:50 +0500

    fraction.py ready

--- lesson3/fraction.py
+++ lesson3/fraction.py
@@ -10,7 +10,20 @@
 
 
 class Fraction:
-    pass
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.numerator}, {self.denominator})"
+
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def __add__(self, other):
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
 
 
 # код для проверки 
