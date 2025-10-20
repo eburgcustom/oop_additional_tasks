33c6a91819413308518970acc165ff9724944319
Ildar <gif8686@gmail.com>
2025-10-20 18:25:19 +0500

    animal_2.py ready

--- lesson4/animal_2.py
+++ lesson4/animal_2.py
@@ -2,14 +2,13 @@
 Допишите код под условия в цикле так, чтобы вывод был корректным
 """
 
-
 class Animal:
 
     def __init__(self, name):
-        pass
+        self.name = name
 
     def walk(self):
-        pass
+        print(f'{self.name} is walking')
 
 
 class Dog(Animal):
@@ -28,5 +27,7 @@ class Cat(Animal):
 animals = [Dog('Dog1'), Dog('Dog2'), Cat('Cat1'), Dog('Dog3')]
 
 for animal in animals:
-    # Должно выводиться Bark или Meow в зависимости от того какой класс
-    pass
+    if isinstance(animal, Dog):
+        animal.bark()
+    elif isinstance(animal, Cat):
+        animal.meow()
